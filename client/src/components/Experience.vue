f981f9243697c774567a2020096e202be32403ce
Janic Beauchemin <jbeauchemin33@gmail.com>
2018-04-29 11:57:40 -0400

    Add print btn

--- client/src/components/Experience.vue
+++ client/src/components/Experience.vue
@@ -48,7 +48,7 @@
             'sumologic',
             'new relic',
             'circleCI',
-            'rabbitMQ'
+            'rabbitMQ',
           ],
         },
         {
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,20 @@
   <div class="home">
     <hero/>
 
+    <v-btn
+      fab
+      color="white"
+      class="print hidden-md-and-down mt-2 mr-2"
+      fixed
+      top
+      right
+      v-on:click.native="_print"
+    >
+      <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+        <path fill="#000000" d="M18,3H6V7H18M19,12A1,1 0 0,1 18,11A1,1 0 0,1 19,10A1,1 0 0,1 20,11A1,1 0 0,1 19,12M16,19H8V14H16M19,8H5A3,3 0 0,0 2,11V17H6V21H18V17H22V11A3,3 0 0,0 19,8Z" />
+      </svg>
+    </v-btn>
+
     <div class="wrapper">
       <div class="stripes">
         <span></span>
@@ -54,6 +68,12 @@
       Education,
       Contact,
     },
+
+    methods: {
+      _print() {
+        window.print();
+      },
+    },
   });
 </script>
 
@@ -101,5 +121,10 @@
     overflow: hidden;
     position: relative;
     z-index: 2;
+  }
+
+  .print {
+    opacity: 0.85;
+    z-index: 10;
   }
 </style>
